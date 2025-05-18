35ee9c4279464f21dd5efc78d8fd6ca85eeb5b8c
stu232917 <stu232917@mail.uni-kiel.de>
2025-05-18 13:55:52 +0200

    add text_extractor and splitter

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
 # Load and Split Text
+import fitz  # PyMuPDF
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def extract_text_from_pdf(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+def split_text(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    return splitter.split_text(text)
